d2347af6d82ab74f42c61a63ff30c085a49c00e5
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-23 20:46:32 +0000

    Added test for fetchPokemon

---
+++ API/api.test.js
@@ -0,0 +1,11 @@
+const fetchPokemon = require('./api')
+
+describe('fetchPokemon', () => {
+  it('Returns a promise of the useful pokemon data.', (done) => {
+    fetchPokemon('charizard')
+      .then((pokemon) => {
+        expect(pokemon.id).toEqual(6);
+        done();
+      });
+  });
+});
