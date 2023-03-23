7949d160231406a6252f8e4d1d2b98952946d04e
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-23 20:19:56 +0000

    Added the types attributes

--- API/api.js
+++ API/api.js
@@ -14,16 +14,14 @@ setTimeout(printEnd, 3000);
 */
 
 function fetchPokemon(pokemonName) {
-   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+   return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
    .then((response) => {
     return response.json()
 })
     .then((data) => {
-        console.log(`Name: ${data.name}`) 
-        console.log(`ID: ${data.id}`) 
-        console.log(`Height: ${data.height}`) 
-        console.log(`Weight: ${data.weight}`) 
-    })
+        const types = data.types.map((element) => element.type.name);
+       return {name: data.name, id: data.id, height: data.height, weight: data.weight, types: types};
+    });
 };
 
 
