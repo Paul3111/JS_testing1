9f4ab519b0cb70730d017a876b41e646625cdb2b
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-23 18:45:49 +0000

    Calling exeternal APIs - Finishing the Pokedex - part1

--- API/api.js
+++ API/api.js
@@ -1,13 +1,30 @@
+/*
 function fetchPokemon(pokemonName) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => console.log(data));
 };
 
-//function printEnd () {
-//    console.log('End of File');
-//};
+function printEnd () {
+    console.log('End of File');
+};
 
-//setTimeout(printEnd, 3000);
+setTimeout(printEnd, 3000);
+
+*/
+
+function fetchPokemon(pokemonName) {
+   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+   .then((response) => {
+    return response.json()
+})
+    .then((data) => {
+        console.log(`Name: ${data.name}`) 
+        console.log(`ID: ${data.id}`) 
+        console.log(`Height: ${data.height}`) 
+        console.log(`Weight: ${data.weight}`) 
+    })
+};
+
 
 module.exports = fetchPokemon;
