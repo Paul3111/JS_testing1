cf633e6f54dfc5f002da910263f0954288279d41
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-23 20:48:12 +0000

    Added a second test for fetchPokemon

--- API/api.test.js
+++ API/api.test.js
@@ -8,4 +8,12 @@ describe('fetchPokemon', () => {
         done();
       });
   });
+
+  it('Returns a promise of the useful pokemon data.', (done) => {
+    fetchPokemon('charizard')
+      .then((pokemon) => {
+        expect(pokemon.height).toEqual(17);
+        done();
+      });
+  });
 });
